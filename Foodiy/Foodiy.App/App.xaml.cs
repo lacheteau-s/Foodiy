e1d8098caf70bfd4e650d4b4440c1b828d476640
Sullivan Lacheteau <lacheteau.s@gmail.com>
2023-03-26 15:16:34 -0400

    Added MAUI project

---
+++ Foodiy/Foodiy.App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Foodiy.App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Foodiy/Foodiy.App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Foodiy.App;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
