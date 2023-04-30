f45dea04d6d41783ecb9434cea43747542acda07
Sullivan Lacheteau <lacheteau.s@gmail.com>
2023-04-30 17:41:36 -0400

    Startup configuration

---
+++ Foodiy/Foodiy.Api/Configuration/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace Foodiy.Api.Configuration;
+
+public static class Startup
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+
+        // Add services to the container.
+
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return builder;
+    }
+
+    public static WebApplication ConfigureMiddlewares(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
