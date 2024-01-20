56e34c24f138b74746cf003bc9464b63a1583639
Sullivan Lacheteau <lacheteau.s@gmail.com>
2024-01-20 02:19:40 -0500

    Basic CollectionView display with hard-coded list

--- Foodiy.App/ViewModels/HomePageViewModel.cs
+++ Foodiy.App/ViewModels/HomePageViewModel.cs
@@ -5,17 +5,21 @@ namespace Foodiy.App.ViewModels;
 
 public partial class HomePageViewModel : ObservableObject
 {
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(CounterText))]
-    private int _count = 0;
+    public IEnumerable<string> Recipes { get; init; }
 
-    public string CounterText => Count == 0 ? "Click me" : $"Clicked {Count} time{(Count > 1 ? "s" : "")} ";
-
-    [RelayCommand]
-    public void IncrementCounter()
+    public HomePageViewModel()
     {
-        Count++;
-
-        SemanticScreenReader.Announce(CounterText);
+        Recipes = new[]
+        {
+            "Chilli con carne",
+            "Burritos",
+            "Salmon with avocado salsa",
+            "Tex-Mex chicken and rice",
+            "Mac & cheese",
+            "Banana bread muffins",
+            "Pasta al pesto",
+            "Mushrooms omelet",
+            "Overnight oats"
+        };
     }
 }
