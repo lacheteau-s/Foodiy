b2ed5aa8d6c3707161d01b830d8c7081d11d9db4
Sullivan Lacheteau <lacheteau.s@gmail.com>
2024-02-01 22:51:29 -0500

    Bind and display recipe name to page title

--- Foodiy.App/ViewModels/RecipePageViewModel.cs
+++ Foodiy.App/ViewModels/RecipePageViewModel.cs
@@ -1,12 +1,21 @@
-﻿using Foodiy.App.Constants;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Foodiy.App.Constants;
 using Foodiy.App.Models;
 
 namespace Foodiy.App.ViewModels;
 
-public class RecipePageViewModel : IQueryAttributable
+public partial class RecipePageViewModel : ObservableObject, IQueryAttributable
 {
+    [ObservableProperty]
+    private string _recipeName = string.Empty;
+
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         var recipe = query[Parameters.RecipeModelParam] as RecipeModel;
+
+        if (recipe != null)
+        {
+            RecipeName = recipe.Name;
+        }
     }
 }
