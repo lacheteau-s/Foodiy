7d6363a5fbf38fb53a7a975731a4ead934009a2e
Sullivan Lacheteau <lacheteau.s@gmail.com>
2024-02-18 23:46:18 -0500

    Recipes controller

---
+++ Foodiy.Api/Controllers/RecipesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foodiy.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class RecipesController : ControllerBase
+{
+    private readonly IEnumerable<string> _recipes = new List<string>
+    {
+        "Recipe 1",
+        "Recipe 2",
+        "Recipe 3",
+        "Recipe 4",
+        "Recipe 5",
+    };
+
+    [HttpGet]
+    public IEnumerable<string> GetRecipes()
+    {
+        return _recipes;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetRecipe(int id)
+    {
+        var recipe = _recipes.ElementAtOrDefault(id);
+
+        if (recipe == null)
+            return NotFound();
+
+        return Ok(recipe);
+    }
+}
