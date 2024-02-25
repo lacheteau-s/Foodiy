827a3d6b8a885952d9a150e331f95635c86d4ece
Sullivan Lacheteau <lacheteau.s@gmail.com>
2024-02-25 00:52:27 -0500

    Cache Azure recipes in memory

--- Foodiy.Api/Services/AzureRecipesProvider.cs
+++ Foodiy.Api/Services/AzureRecipesProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Azure.Storage.Files.Shares;
 using Foodiy.Api.Configuration.Options;
 using Foodiy.Api.Models;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using System.Text.Json;
 
@@ -8,14 +9,29 @@ namespace Foodiy.Api.Services;
 
 public class AzureRecipesProvider : IRecipesProvider
 {
+    private const string _cacheKey = "recipes";
+
     private readonly AzureStorageOptions _configuration;
 
-    public AzureRecipesProvider(IOptions<AzureStorageOptions> storageConfig)
+    private readonly IMemoryCache _memoryCache;
+
+    public AzureRecipesProvider(IOptions<AzureStorageOptions> storageConfig, IMemoryCache memoryCache)
     {
         _configuration = storageConfig.Value;
+        _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
     }
 
     public async Task<IEnumerable<RecipeDetailsModel>> GetRecipesAsync(CancellationToken cancellationToken = default)
+    {
+        if (_memoryCache.TryGetValue<IEnumerable<RecipeDetailsModel>>(_cacheKey, out var recipes))
+            return recipes;
+
+        recipes = await DownloadRecipesFromAzureStorageAsync(cancellationToken);
+
+        return _memoryCache.Set(_cacheKey, recipes);
+    }
+
+    private async Task<IEnumerable<RecipeDetailsModel>> DownloadRecipesFromAzureStorageAsync(CancellationToken cancellationToken)
     {
         var fileShare = new ShareClient(_configuration.ConnectionString, _configuration.ShareName);
 
